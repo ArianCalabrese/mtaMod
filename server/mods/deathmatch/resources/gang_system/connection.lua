ea191dcee938071fea8f1f629286b0ec35a4c7a0
ArianCalabrese <ariancalabrese@hotmail.com>
2021-05-24 00:57:39 -0300

    basic stuff

---
+++ server/mods/deathmatch/resources/gang_system/connection.lua
@@ -0,0 +1,16 @@
+--[[
+	You can choose between using SQLite or MySQL to store gang system related data
+
+	If you decide to use MySQL, you will have to change the variables below to your MySQL server.
+
+	In case you decide to use SQLite, you must remember to unzip the resource into a folder, since the database will be created inside the resource folder.
+--]]
+
+connectionType = "sqlite"
+
+-- MySQL details
+-- mysql_host = ""
+-- mysql_port = ""
+-- mysql_user = ""
+-- mysql_password = ""
+-- mysql_database = ""
